8c85d292d95e25de456061e8f99393aee1bea570
Rishi <rishi@192-241-61-62.ip.ctc.biz>
2017-03-05 15:35:43 -0600

    changes

---
+++ RendezWho/RendezWho/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, HttpResponse
+
+def home(request):
+    return render(request,'home.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+@login_required
+def logout(request):
+    return render(request, 'logout.html')
+
+@login_required
+def my_profile(request):
+    return render(request, 'myProfile.html')
+
+@login_required
+def friend_profile(request):
+    return render(request, 'friendProfile.html')
+
+@login_required
+def view_newsfeed(request):
+    return render(request, 'newsfeed.html')
+
+@login_required
+def view_connections(request):
+    return render(request, 'connections.html')
+
+@login_required
+def send_match_request(request):
+    return render(request, 'request.html')
+
+@login_required
+def search(request):
+    return render(request, 'search.html')
+
+@login_required
+def settings(request):
+    return render(request, 'settings.html')
--- RendezWho/settings.py
+++ RendezWho/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'RendezWho.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'RendezWho/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'RendezWho.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'RendezWho/db.sqlite3'),
     }
 }
 
