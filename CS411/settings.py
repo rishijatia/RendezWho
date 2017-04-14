55ac1d34c5fe14fe8b238ab2d5b2fbd336b0b559
rishijatia <rishi.jatia96@gmail.com>
2017-04-14 11:48:06 -0500

    trying

--- CS411/settings.py
+++ CS411/settings.py
@@ -138,10 +138,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+MIGRATION_MODULES = {"RendezWho": None}
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '522860969064-qg3liitd7mqa5qsdoov9mri5rnfnmimp.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'JyfNSf4Owb53XikA0gesG6yT'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
