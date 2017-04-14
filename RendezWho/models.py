2c64eb03c9ad1a7825d559f91fe45e09ac2e7fcf
rishijatia <rishi.jatia96@gmail.com>
2017-04-13 19:54:28 -0500

    stuff

--- RendezWho/models.py
+++ RendezWho/models.py
@@ -7,17 +7,13 @@ class UserApp(models.Model):
     user = models.OneToOneField(User,primary_key=True)
     connections = models.ManyToManyField('self',related_name='friends')
 
-class Location(models.Model):
-    DEFAULT_PK=1
-    name = models.CharField(primary_key=True,max_length=50)
-
 class Schedule_Entry(models.Model):
     entryID = models.BigAutoField(primary_key=True)
     activity = models.CharField(max_length=50)
     start_time = models.TimeField(auto_now_add=True,blank=True)
     end_time = models.TimeField(auto_now_add=True, blank=True)
     date = models.DateField(auto_now_add=True, blank=True)
-    located = models.ForeignKey(Location,related_name='loc',default=Location.DEFAULT_PK)
+    located = models.CharField(primary_key=True,max_length=50)
     owner = models.ForeignKey(UserApp,related_name="owns")
 
 class Meeting(models.Model):
@@ -28,5 +24,5 @@ class Meeting(models.Model):
     start_time = models.TimeField(auto_now_add=True, blank=True)
     date = models.DateField(auto_now_add=True, blank=True)
     participants = models.ManyToManyField(UserApp,related_name="participants")
-    is_at = models.ForeignKey(Location,related_name='at',default=Location.DEFAULT_PK)
+    is_at = models.CharField(primary_key=True,max_length=50)
     end_time = models.TimeField(auto_now_add=True, blank=True)
