a0439aec755da4ed96ae157012ae70f93fe855b8
rishijatia <rishi.jatia96@gmail.com>
2017-04-19 18:58:55 -0500

    test

--- RendezWho/views.py
+++ RendezWho/views.py
@@ -23,9 +23,9 @@ def createUser(request):
     uapp = UserApp(user=request.user)
     uapp.save()
   user_id = request.user.social_auth.filter(provider='google-oauth2')[1]
-  formatted_time = datetime.datetime.now().isoformat()
-  formatted_time=formatted_time[:formatted_time.rfind('.')]
-  #formatted_time = "2017-10-02T23:30:00+05:30"
+  #formatted_time = datetime.datetime.now().isoformat()
+  #formatted_time=formatted_time[:formatted_time.rfind('.')]
+  formatted_time = "2017-06-30T23:30:00+05:30"
   #dict2 = {'start': {'dateTime':formatted_time,'timeZone':''}}
   response = requests.get(
     'https://www.googleapis.com/calendar/v3/calendars/primary/events',
