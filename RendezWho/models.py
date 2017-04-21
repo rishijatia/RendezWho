cf3bb25d5c762e316d4f72f5e18c85fd39f02752
rishijatia <rishi.jatia96@gmail.com>
2017-04-20 19:28:50 -0500

    no script

--- RendezWho/models.py
+++ RendezWho/models.py
@@ -16,9 +16,9 @@ class CRequest(models.Model):
 class Schedule_Entry(models.Model):
     entryID = models.BigAutoField(primary_key=True)
     activity = models.CharField(max_length=500)
-    start_time = models.DateTimeField(auto_now_add=True,blank=True)
-    end_time = models.DateTimeField(auto_now_add=True, blank=True)
-    date = models.DateField(auto_now_add=True, blank=True)
+    start_time = models.DateTimeField(blank=True)
+    end_time = models.DateTimeField(blank=True)
+    date = models.DateField(blank=True)
     located = models.CharField(max_length=500)
     owner = models.ForeignKey(UserApp,related_name="owns")
 
