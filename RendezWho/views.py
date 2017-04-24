da09cf25e5b504b9a74606026d15a9e0d3fffe8d
rishijatia <rishi.jatia96@gmail.com>
2017-04-24 17:33:43 -0500

    kladslaks

--- RendezWho/views.py
+++ RendezWho/views.py
@@ -240,7 +240,7 @@ def view_newsfeed(request):
     top_twenty_newsfeed = []
     advanced_query = Meeting.objects.filter(Q(approved=True) & (Q(requester__in=(UserApp.objects.filter(user=request.user))) 
     | Q(participants__in=(UserApp.objects.filter(user=request.user).only("connections"))))).order_by('-start_time')[:20]
-    print (UserApp.objects.filter(user=request.user).values('connections'))['connections']
+    #print (UserApp.objects.filter(user=request.user).values('connections'))['connections']
     for entry in advanced_query:
       temp = {}
       temp['name1'] = entry.participants.all()[0].user.username
