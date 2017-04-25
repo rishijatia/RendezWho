f6cda86f7e1ef083c80fbcf1b776e52b7035ca84
rishijatia <rishi.jatia96@gmail.com>
2017-04-25 11:35:06 -0500

    tanvi-changes

--- CS411/settings.py
+++ CS411/settings.py
@@ -143,4 +143,5 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(os.path.join(PROJECT_ROOT,'static'),)
-STATICFILES_STORAGE='django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+#STATICFILES_STORAGE='django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage''
