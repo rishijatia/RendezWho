5d9fef054f76ee27fd598df4be21a0f496d7c625
rishijatia <rishi.jatia96@gmail.com>
2017-04-24 19:26:02 -0500

    kladslaks

--- RendezWho/views.py
+++ RendezWho/views.py
@@ -240,8 +240,8 @@ def view_newsfeed(request):
     cnns = []
 
     top_twenty_newsfeed = []
-    advanced_query = Meeting.objects.filter(Q(approved=True) & (Q(requester__in=([UserApp.objects.filter(user_id=val)[0].user for val in UserApp.objects.filter(user=request.user)[0].connections.all().values_list('user',flat=True)])) 
-    | Q(participants__in=([UserApp.objects.filter(user_id=val)[0].user for val in UserApp.objects.filter(user=request.user)[0].connections.all().values_list('user',flat=True)])))).order_by('-start_time')[:20]
+    advanced_query = Meeting.objects.filter(Q(approved=True) & (Q(requester__in=([UserApp.objects.filter(user_id=val)[0] for val in UserApp.objects.filter(user=request.user)[0].connections.all().values_list('user',flat=True)])) 
+    | Q(participants__in=([UserApp.objects.filter(user_id=val)[0] for val in UserApp.objects.filter(user=request.user)[0].connections.all().values_list('user',flat=True)])))).order_by('-start_time')[:20]
     for entry in advanced_query:
       temp = {}
       temp['name1'] = entry.participants.all()[0].user.username
